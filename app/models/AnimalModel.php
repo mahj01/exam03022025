3a2d0c88f76a2d644d6603ec5c1cced21cbc0a39
mahj01 <mahavonjy21@gmail.com>
2025-02-03 21:35:48 +0300

    Update AnimalModel.php

--- app/models/AnimalModel.php
+++ app/models/AnimalModel.php
@@ -17,7 +17,6 @@ class AnimalModel extends BaseModel
 
     public function getDeadCountByDate($date){
         $sql = "SELECT count(*) nbMorts from elevage_AnimalDecede where dateDeces <= ?";
-
         $stmt = $this->db->prepare($sql);
         $stmt->bindValue(1,$date);
         return $stmt->fetch();
@@ -25,19 +24,38 @@ class AnimalModel extends BaseModel
 
     public function getDernierJourAlim($idAnimal){
         $sql = "select dateAlimentation from elevage_HistoriqueAlimentation where idAnimal=?";
+        $stmt = $this->db->prepare($sql);
         $stmt->bindValue(1,$idAnimal);
         return $stmt->fetch();
     }
 
     public function getPoidsInit($idAnimal){
         $sql = "select PoidsInitial from elevage_Animal where idAnimal=?";
+        $stmt = $this->db->prepare($sql);
         $stmt->bindValue(1,$idAnimal);
         return $stmt->fetch();
     }
 
-    public function getAlimentation($idAnimal){
-        
+    public function getAlimentation($idAnimal,$date){
+        $sql = "select coalesce(id,0) alimentation from elevage_HistoriqueAlimentation where idAnimal = ? and dateAlimentation = ?";
+        $stmt = $this->db->prepare($sql);    
+        $stmt->bindValue(1,$idAnimal);
+        $stmt->bindValue(2,$date);
+        return $stmt->fetch();
     }
+
+    public function getPerteParJour($idAnimal){
+        $sql = "select PerteParJour from elevage_Espece EE join (select idEspece from elevage_Animal where id = ?) EA on EE.id = EA.idEspece";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(1,$idAnimal);
+        return $stmt->fetch();
+    }
+
+    
+
+
+
+
 
     
 }
