0e3fbd1883cd37ebeac690070a32232b244563f1
mahj01 <mahavonjy21@gmail.com>
2025-02-04 01:26:24 +0300

    tsy aiko tsony

--- app/models/AnimalModel.php
+++ app/models/AnimalModel.php
@@ -98,6 +98,20 @@ class AnimalModel extends BaseModel
 
         return $prixParKg*$poids;
     }
+
+    public function getEstimationByEspece($date,$idEspece){
+        $somme = 0;
+        $sql = "SELECT idAnimal from elevage_Animal where idEspece = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(1,$idEspece);
+        $rs = $stmt->execute()->fetchAll();
+        for($i=0; $i<count($rs);$i++){
+            $somme += $this->getEstimationValeur($rs[$i]["idAnimal"]);
+        }
+        return $somme;
+
+    }
+
     
 
 
--- app/models/TransactionCaisseModel.php
+++ app/models/TransactionCaisseModel.php
@@ -19,6 +19,27 @@ class TransactionCaisseModel extends BaseModel
         return $stmt->fetch();
     }
 
-    
+    public function getDepensesTotal(){
+        $sql = "SELECT sum(montant) montant from elevage_TransactionCaisse where montant < 0";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute()->fetch();
+        
+    }
+
+    public function getRevenusTotal(){
+        $sql = "SELECT sum(montant) montant from elevage_TransactionCaisse where montant > 0";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute()->fetch();
+    }
+
+    public function evolutionCapital($date){
+        $sql = "SELECT montant from elevage_TransactionCaisse where dateTransaction >= ? and dateTransaction <= ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(1,$date);
+        $stmt->bindValue(2,$date);
+        return $stmt->execute()->fetch();
+
+    }
+
 
 }
