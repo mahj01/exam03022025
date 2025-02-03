81a3372a85d469ec6e80e2d0305f80b1bdb20617
tonioluc <antoniorandria3342@gmail.com>
2025-02-03 12:34:17 +0300

    faire marcher

--- app/config/config.php
+++ app/config/config.php
@@ -70,10 +70,10 @@ if (Debugger::$showBar && php_sapi_name() !== 'cli') {
 return [
 	'database' => [
 		// uncomment the below 4 lines for mysql
-		 'host' => 'localhost:8889',
-		 'dbname' => 'tpflight',
+		 'host' => 'localhost',
+		 'dbname' => 'immobilier',
 		 'user' => 'root',
-		 'password' => 'root'
+		 'password' => ''
 
 		// uncomment the following line for sqlite
 		// 'file_path' => __DIR__ . $ds . '..' . $ds . 'database.sqlite'
