81e59a9e45948c1f4bab5d970b5419563fb5231c
sarobidyBryan <raybryansarobidy@gmail.com>
2025-02-03 20:02:29 +0300

    page template

---
+++ app/views/template.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+</head>
+<body>
+    <div>ireto ilay partie tsy miova</div>
+    <main>
+        <!-- <?= $content ?> ato ilay partie miova -->
+    </main>
+    <footer>
+        <div>ireto ilay partie tsy miova</div>
+    </footer>
+</body>
+</html>
