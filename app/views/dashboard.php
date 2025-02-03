c20f129845ade8a89512ec04ee994a2c62349bd9
tonioluc <antoniorandria3342@gmail.com>
2025-02-03 23:07:11 +0300

    add files

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -80,11 +80,7 @@
     <div class="container">
         <div class="dashboard">
             <div>
-                <h3>Situation actuelle</h3>
-                <p><strong>Capital initial:</strong> 10000€</p>
-                <p><strong>Capital actuel:</strong> <span id="capital">10000</span>€</p>
-                <p><strong>Nombre d'animaux:</strong> <span id="nb-animals">3</span></p>
-                <p><strong>Poids total des animaux:</strong> <span id="total-weight">2500</span> kg</p>
+                <?php include('situation-actuelle.php'); ?>
             </div>
             <div>
                 <h3>Choisir une date</h3>
